5001b41bb3463e4ae5ab5b5c1d7919f2b9df1a6a
KilianCadiou <kiliancadiou@gmail.com>
2025-03-04 14:47:35 +0100

    responsive

--- script.js
+++ script.js
@@ -53,11 +53,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.querySelector(".nav-toggle");
-    const navMenu = document.querySelector("nav ul");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const toggleButton = document.querySelector(".nav-toggle");
+//     const navMenu = document.querySelector("nav ul");
 
-    toggleButton.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
-    });
-});
+//     toggleButton.addEventListener("click", function () {
+//         navMenu.classList.toggle("show");
+//     });
+// });
